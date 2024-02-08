aa6f142072d1647122fc6d76b0e08ea22c0f26c9
aceiny <a_zeraibi@estin.dz>
2024-02-09 00:52:49 +0100

    update readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { AlertDialog } from "@radix-ui/react-alert-dialog";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <p>workin</p>
+      <Button>Click me</Button>
     </main>
   );
 }
