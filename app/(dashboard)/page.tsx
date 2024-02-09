00f7899bf9c3fdec505fb7a94d9e02ae4b47082c
aceiny <a_zeraibi@estin.dz>
2024-02-09 02:17:59 +0100

    add basic layout

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { SignIn, UserButton } from "@clerk/nextjs";
 import { AlertDialog } from "@radix-ui/react-alert-dialog";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <p>workin</p>
+      <p>dahsboard main</p>
     </main>
   );
 }
