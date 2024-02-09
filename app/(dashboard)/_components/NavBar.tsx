85e21830660d37ce1db5661da96defe66b4ca130
aceiny <a_zeraibi@estin.dz>
2024-02-09 20:17:10 +0100

    search input

--- app/(dashboard)/_components/NavBar.tsx
+++ app/(dashboard)/_components/NavBar.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { UserButton } from '@clerk/nextjs';
-import React from 'react'
-
+import { Search } from 'lucide-react';
+import React , {useState} from 'react'
 const NavBar = () => {
   return (
     <div className=' w-full flex items-center gap-x-4 p-5 bg-gray-500 '>
-      <div className='hidden lg:flex lg:flex-1 bg-yellow'>
-        <input type="text" />
-        <p>search</p>
+      <div className='hidden lg:flex lg:flex-1'>
+        <div className='w-[510px] gap-1 p-3 bg-white rounded-md flex items-center'>
+          <Search/>
+          <input type="text" className=' outline-none w-full' placeholder='search boards'/>
+        </div>
       </div>
       <UserButton/>
     </div>
